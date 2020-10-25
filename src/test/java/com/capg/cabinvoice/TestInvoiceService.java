db20e4759c40c535c88aa90f0d686adf1cd8f908
Rajan-rkg9 <amirajangupta@gmail.com>
2020-10-25 13:21:22 +0530

    [Rajan-rkg9] Optimixed the code using @Before annotation

--- src/test/java/com/capg/cabinvoice/TestInvoiceService.java
+++ src/test/java/com/capg/cabinvoice/TestInvoiceService.java
@@ -2,13 +2,21 @@ package com.capg.cabinvoice;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestInvoiceService {
+	InvoiceGenerator invoiceGenerator = null;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		invoiceGenerator = new InvoiceGenerator();	
+	}
+	
 	@Test
 	public void givenDistanceAndTime_ShouldReturnTotalFare()
 	{
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance,time);
@@ -18,7 +26,6 @@ public class TestInvoiceService {
 	@Test
 	public void givenLessDistanceOrTime_ShouldReturnMinFare()
 	{
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance,time);
